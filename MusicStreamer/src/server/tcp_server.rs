2f36efd24469bd6bff684fac59b8e6ea87958d3b
DESKTOP-84BFPQT\james <james.wang.w@gmail.com>
2023-02-07 22:41:44 -0500

    adding threadpool to handle requests

--- MusicStreamer/src/server/tcp_server.rs
+++ MusicStreamer/src/server/tcp_server.rs
@@ -2,6 +2,7 @@ use std::io::{Read, Write};
 use std::net::{Incoming, TcpListener, TcpStream};
 use std::ops::Deref;
 use crate::server::music_commands::MusicInfo;
+use threadpool::ThreadPool;
 
 pub(crate) trait IncomingHandler {
     fn new(t: String) -> Self;
@@ -9,10 +10,14 @@ pub(crate) trait IncomingHandler {
 }
 
 pub fn listen_on(port: u32, incoming_handler: MusicInfo) {
+    let pool = &ThreadPool::new(4);
     let conn_listener: TcpListener = TcpListener::bind(format!("0.0.0.0:{port}")).unwrap();
     println!("Running on port {port}");
+    incoming_handler.clone();
     for stream in conn_listener.incoming() {
         println!("Connection established");
-        incoming_handler.handle(&stream.unwrap())
+        let in_handler = incoming_handler.clone();
+        pool.execute(move|| in_handler.handle(&stream.unwrap()));
+        println!("Handling next connection")
     }
 }
